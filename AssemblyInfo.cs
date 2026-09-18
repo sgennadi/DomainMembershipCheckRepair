1d9cd41cc8ed35138d63c9d97fa3533279d21f6e
sgennadi <51334810+sgennadi@users.noreply.github.com>
2026-09-18 14:41:00 +0300

    Add AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Domain Membership Check & Repair")]
+[assembly: AssemblyDescription("Windows domain membership, trust, join and computer account recovery utility")]
+[assembly: AssemblyProduct("DomainMembershipCheckRepair")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: ComVisible(false)]
