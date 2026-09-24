c304287be8778101fe87c3d4c6ffd7f124e370ea
sgennadi <51334810+sgennadi@users.noreply.github.com>
2026-09-25 01:40:14 +0300

    Fix Windows command quoting

--- Core/OfflineDomainJoinService.cs
+++ Core/OfflineDomainJoinService.cs
@@ -73,7 +73,7 @@ namespace DomainMembershipCheckRepair
 
         private static string Quote(string value)
         {
-            return """ + (value ?? String.Empty).Replace(""", "\"") + """;
+            return "\"" + (value ?? String.Empty).Replace("\"", "\\\"") + "\"";
         }
     }
 }
