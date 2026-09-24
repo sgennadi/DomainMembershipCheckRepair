bf0fd32ea651a591ef00122581ed2c3fdb31ef58
sgennadi <51334810+sgennadi@users.noreply.github.com>
2026-09-25 01:16:22 +0300

    Bump version to 1.3.0

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -8,7 +8,7 @@ namespace DomainMembershipCheckRepair
 {
     internal static class VersionInfo
     {
-        internal const string ProductVersion = "1.2.1";
-        internal const string AssemblyVersion = "1.2.1.0";
+        internal const string ProductVersion = "1.3.0";
+        internal const string AssemblyVersion = "1.3.0.0";
     }
 }
